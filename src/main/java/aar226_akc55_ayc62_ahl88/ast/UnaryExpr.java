71082351a2484e32a4cce3da0aae20f25d268f19
Angela Cui <ayc62@cornell.edu>
2023-02-12 17:21:10 -0500

    added arg and unop

---
+++ src/main/java/aar226_akc55_ayc62_ahl88/ast/UnaryExpr.java
@@ -0,0 +1,22 @@
+package aar226_akc55_ayc62_ahl88.ast;
+
+import aar226_akc55_ayc62_ahl88.src.edu.cornell.cs.cs4120.util.CodeWriterSExpPrinter;
+
+class UnaryExpr extends Expr {
+    private String uop;
+    private Expr e;
+
+    public UnaryExpr(String uop, Expr e) {
+        this.uop = uop;
+        this.e = e;
+        this.type = Exprs.Uop;
+    }
+
+    public void prettyPrinter(CodeWriterSExpPrinter p) {
+        p.startList();
+        p.printAtom(uop);
+        e.prettyPrint(p);
+        p.endList();
+    }
+
+}
