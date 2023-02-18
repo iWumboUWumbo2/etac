fb76bd286a305f263032ee044a58194517121c2d
Andrew <akc55@cornell.edu>
2023-02-18 15:57:25 -0500

    unop pretty Print

--- src/main/java/aar226_akc55_ayc62_ahl88/newast/expr/unop/UnopExpr.java
+++ src/main/java/aar226_akc55_ayc62_ahl88/newast/expr/unop/UnopExpr.java
@@ -3,6 +3,7 @@ package aar226_akc55_ayc62_ahl88.newast.expr.unop;
 import aar226_akc55_ayc62_ahl88.newast.expr.Expr;
 import aar226_akc55_ayc62_ahl88.newast.expr.binop.BinopEnum;
 import aar226_akc55_ayc62_ahl88.newast.expr.unop.UnopEnum;
+import aar226_akc55_ayc62_ahl88.src.edu.cornell.cs.cs4120.util.CodeWriterSExpPrinter;
 
 public abstract class UnopExpr extends Expr {
     Expr e;
@@ -20,5 +21,24 @@ public abstract class UnopExpr extends Expr {
         e = in;
         unopType = u;
     }
-//    public abstract void prettyPrint(CodeWriterSExpPrinter p);
+    @Override
+    public void prettyPrint(CodeWriterSExpPrinter p) {
+
+        String out;
+
+        switch (unopType){
+            case NOT:
+                out = "!";
+                break;
+            case INT_NEG:
+                out = "-";
+                break;
+            default:
+                throw new Error(getLine()+":" + getColumn() + "Error in Unop Case Statement");
+        }
+        p.startList();
+        p.printAtom(out);
+        e.prettyPrint(p);
+        p.endList();
+    }
 }
