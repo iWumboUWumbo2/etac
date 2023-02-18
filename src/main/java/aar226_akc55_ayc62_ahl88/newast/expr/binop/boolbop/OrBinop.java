5dd245cb604bc14108a00d585668417615a22cae
alanli2001 <alanli2001@users.noreply.github.com>
2023-02-18 15:28:36 -0500

    or binop

--- src/main/java/aar226_akc55_ayc62_ahl88/newast/expr/binop/boolbop/OrBinop.java
+++ src/main/java/aar226_akc55_ayc62_ahl88/newast/expr/binop/boolbop/OrBinop.java
@@ -1,4 +1,10 @@
 package aar226_akc55_ayc62_ahl88.newast.expr.binop.boolbop;
 
+import aar226_akc55_ayc62_ahl88.newast.expr.Expr;
+import aar226_akc55_ayc62_ahl88.newast.expr.binop.BinopEnum;
+
 public class OrBinop extends LogicalBinop {
+    public OrBinop (Expr in1, Expr in2, int l, int c) {
+        super(BinopEnum.OR, in1, in2, l, c);
+    }
 }
