f675c75bc34454a948150b41e008ae10f19fedd7
alanli2001 <alanli2001@users.noreply.github.com>
2023-02-18 15:28:09 -0500

    neq binop

--- src/main/java/aar226_akc55_ayc62_ahl88/newast/expr/binop/boolbop/NotEqualsBinop.java
+++ src/main/java/aar226_akc55_ayc62_ahl88/newast/expr/binop/boolbop/NotEqualsBinop.java
@@ -1,4 +1,10 @@
 package aar226_akc55_ayc62_ahl88.newast.expr.binop.boolbop;
 
+import aar226_akc55_ayc62_ahl88.newast.expr.Expr;
+import aar226_akc55_ayc62_ahl88.newast.expr.binop.BinopEnum;
+
 public class NotEqualsBinop extends EquivalenceBinop {
+    public NotEqualsBinop (Expr in1, Expr in2, int l, int c) {
+        super(BinopEnum.NOTEQUALS, in1, in2, l, c);
+    }
 }
