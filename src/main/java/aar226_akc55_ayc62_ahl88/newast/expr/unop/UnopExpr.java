9adb468dbd8ffd4d089c2c2a9645c2e7b699b81e
Arun Raman <aar226@cornell.edu>
2023-02-18 15:41:27 -0500

    hopefully finished unop

--- src/main/java/aar226_akc55_ayc62_ahl88/newast/expr/unop/UnopExpr.java
+++ src/main/java/aar226_akc55_ayc62_ahl88/newast/expr/unop/UnopExpr.java
@@ -1,7 +1,24 @@
-package aar226_akc55_ayc62_ahl88.newast.
+package aar226_akc55_ayc62_ahl88.newast.expr.unop;
 
-import aar226_akc55_ayc62_ahl88.newast.expr.Expr;*;
+import aar226_akc55_ayc62_ahl88.newast.expr.Expr;
+import aar226_akc55_ayc62_ahl88.newast.expr.binop.BinopEnum;
+import aar226_akc55_ayc62_ahl88.newast.expr.unop.UnopEnum;
 
 public abstract class UnopExpr extends Expr {
+    Expr e;
+    UnopEnum unopType;
 
+    /**
+     * Abstract class for all Unary Expressions
+     * @param b  Unary Enum
+     * @param in Expression
+     * @param l line number
+     * @param c column number
+     */
+    public UnopExpr(UnopEnum u, Expr in, int l, int c) {
+        super(l,c);
+        e = in;
+        unopType = u;
+    }
+//    public abstract void prettyPrint(CodeWriterSExpPrinter p);
 }
--- src/main/java/aar226_akc55_ayc62_ahl88/newast/expr/unop/booluop/NotUnop.java
+++ src/main/java/aar226_akc55_ayc62_ahl88/newast/expr/unop/booluop/NotUnop.java
@@ -1,4 +1,18 @@
 package aar226_akc55_ayc62_ahl88.newast.expr.unop.booluop;
 
+import aar226_akc55_ayc62_ahl88.newast.expr.Expr;
+import aar226_akc55_ayc62_ahl88.newast.expr.binop.BinopEnum;
+import aar226_akc55_ayc62_ahl88.newast.expr.unop.UnopEnum;
+
 public class NotUnop extends BooleanUnop {
+    /**
+     * Class for Boolean Negation Unary Operator
+     *
+     * @param in Expression
+     * @param l line number
+     * @param c column number
+     */
+    public NotUnop(Expr in, int l, int c) {
+        super(UnopEnum.NOT, in, l, c);
+    }
 }
