7c655ec4de4bd6b0d39083b85a390fa1453e7d39
alanli2001 <alanli2001@users.noreply.github.com>
2023-02-18 15:19:23 -0500

    andbinop

--- src/main/java/aar226_akc55_ayc62_ahl88/newast/expr/binop/boolbop/AndBinop.java
+++ src/main/java/aar226_akc55_ayc62_ahl88/newast/expr/binop/boolbop/AndBinop.java
@@ -1,4 +1,9 @@
 package aar226_akc55_ayc62_ahl88.newast.expr.binop.boolbop;
+import aar226_akc55_ayc62_ahl88.newast.expr.Expr;
+import aar226_akc55_ayc62_ahl88.newast.expr.binop.BinopEnum;
 
 public class AndBinop extends LogicalBinop {
+    public AndBinop (Expr e1, Expr e2, int l, int c) {
+        super(BinopEnum.AND, in1, in2, l, c);
+    }
 }
