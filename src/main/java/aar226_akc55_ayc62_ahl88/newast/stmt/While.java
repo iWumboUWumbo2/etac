e32fa6ba8fd9d20b22de5bf81b075a51b01d91b7
alanli2001 <alanli2001@users.noreply.github.com>
2023-02-18 17:57:02 -0500

    while

--- src/main/java/aar226_akc55_ayc62_ahl88/newast/stmt/While.java
+++ src/main/java/aar226_akc55_ayc62_ahl88/newast/stmt/While.java
@@ -4,11 +4,21 @@ import aar226_akc55_ayc62_ahl88.ast.Expr;
 import aar226_akc55_ayc62_ahl88.ast.Stmt;
 import aar226_akc55_ayc62_ahl88.src.edu.cornell.cs.cs4120.util.CodeWriterSExpPrinter;
 
+/**
+ * While Class for AST node
+ */
 public class While extends Stmt {
     private Expr expr;
     private Stmt stmt;
 
-    public While(Expr expr, Stmt s) {
+    /**
+     * @param expr Expression
+     * @param s Statement
+     * @param l line number
+     * @param c colunn number
+     */
+    public While(Expr expr, Stmt s, int l, int c) {
+        super(l,c);
         this.expr = expr;
         stmt = s;
     }
