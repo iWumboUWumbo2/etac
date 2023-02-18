ef7543ed56eba616e9f2911d93450dc0e496dc98
Andrew <akc55@cornell.edu>
2023-02-18 16:07:05 -0500

    updated Length and FunctionCall

--- src/main/java/aar226_akc55_ayc62_ahl88/newast/expr/FunctionCall.java
+++ src/main/java/aar226_akc55_ayc62_ahl88/newast/expr/FunctionCall.java
@@ -1,5 +1,25 @@
 package aar226_akc55_ayc62_ahl88.newast.expr;
 
-public class FunctionCall extends Expr {
+import aar226_akc55_ayc62_ahl88.ast.Id;
+import aar226_akc55_ayc62_ahl88.src.edu.cornell.cs.cs4120.util.CodeWriterSExpPrinter;
 
+import java.util.ArrayList;
+
+public class FunctionCall extends Expr {
+    Id id;
+    ArrayList<Expr> args;
+
+    public FunctionCall(Id i, ArrayList<Expr> inArgs, int l, int c) {
+        super (l, c);
+        id = i;
+        args = inArgs;
+    }
+    @Override
+    public void prettyPrint(CodeWriterSExpPrinter p) {
+        p.startList();
+        id.prettyPrint(p);
+        args.forEach(e -> e.prettyPrint(p));
+        p.endList();
+
+    }
 }
--- src/main/java/aar226_akc55_ayc62_ahl88/newast/expr/Length.java
+++ src/main/java/aar226_akc55_ayc62_ahl88/newast/expr/Length.java
@@ -1,5 +1,21 @@
 package aar226_akc55_ayc62_ahl88.newast.expr;
 
-public class Length extends Expr {
+import aar226_akc55_ayc62_ahl88.src.edu.cornell.cs.cs4120.util.CodeWriterSExpPrinter;
 
+public class Length extends Expr {
+    Expr arg;
+
+    public Length(Expr e, int l, int c) {
+        super(l,c);
+        arg = e;
+    }
+    @Override
+    public void prettyPrint(CodeWriterSExpPrinter p){
+        p.startList();
+        p.printAtom("length");
+        arg.prettyPrint(p);
+        p.endList();
+    }
+
+    // TypeCheck Arg is Array
 }
