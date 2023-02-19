ec4446dc1f3b5c724f97dd88e304625dcfcdf282
Angela Cui <ayc62@cornell.edu>
2023-02-19 13:22:39 -0500

    definitions folder

---
+++ src/main/java/aar226_akc55_ayc62_ahl88/newast/definitions/Definition.java
@@ -0,0 +1,31 @@
+package aar226_akc55_ayc62_ahl88.newast.definitions;
+
+import aar226_akc55_ayc62_ahl88.src.edu.cornell.cs.cs4120.util.CodeWriterSExpPrinter;
+
+public interface Definition {
+//    private Method method;
+//    private Globdecl globdecl;
+//
+//    public Definition( Method m){
+//        this.method = m;
+//        this.globdecl = null;
+//    }
+//
+//    public Definition(Globdecl globdecl) {
+//        this.globdecl = globdecl;
+//        this.method = null;
+//    }
+//
+//    public String toString(){
+//        return (method == null) ? globdecl.toString() : method.toString();
+//    }
+//
+//    @Override
+//    public void prettyPrint(CodeWriterSExpPrinter p) {
+//        p.startList();
+//        if (method == null) globdecl.prettyPrint(p);
+//        else method.prettyPrint(p);
+//        p.endList();
+//    }
+    void prettyPrint(CodeWriterSExpPrinter p);
+}
---
+++ src/main/java/aar226_akc55_ayc62_ahl88/newast/definitions/Globdecl.java
@@ -0,0 +1,37 @@
+package aar226_akc55_ayc62_ahl88.newast.definitions;
+
+import aar226_akc55_ayc62_ahl88.ast.Decl;
+import aar226_akc55_ayc62_ahl88.ast.Expr;
+import aar226_akc55_ayc62_ahl88.ast.Value;
+import aar226_akc55_ayc62_ahl88.src.edu.cornell.cs.cs4120.util.CodeWriterSExpPrinter;
+
+public class Globdecl implements Definition {
+    private Decl decl;
+    private Value value;
+
+    public Globdecl (Decl d, Value v) {
+        decl = d;
+        value = v;
+    }
+
+    public String toString(){
+        String build = "";
+        if (value != null){
+//            System.out.println("IM HERE");
+            build +=  "( " + decl.toString() + " " +value.toString() +  " )";
+        }else{
+            build +=  "( " + decl.toString() +  " )";
+        }
+        return build;
+    }
+    @Override
+    public void prettyPrint(CodeWriterSExpPrinter p) {
+        p.startList();
+        decl.prettyPrint(p);
+        if (value != null){
+            ((Expr) value).prettyPrint(p);
+        }
+        p.endList();
+    }
+
+}
