80a9ffc9e912a95e5627b01a1908167b7b18d971
alanli2001 <alanli2001@users.noreply.github.com>
2023-02-19 15:21:35 -0500

    patched method

--- src/main/java/aar226_akc55_ayc62_ahl88/newast/definitions/Method.java
+++ src/main/java/aar226_akc55_ayc62_ahl88/newast/definitions/Method.java
@@ -2,7 +2,9 @@ package aar226_akc55_ayc62_ahl88.newast.definitions;
 
 import aar226_akc55_ayc62_ahl88.newast.*;
 import aar226_akc55_ayc62_ahl88.src.edu.cornell.cs.cs4120.util.CodeWriterSExpPrinter;
-
+import aar226_akc55_ayc62_ahl88.newast.declarations.Decl;
+import aar226_akc55_ayc62_ahl88.newast.expr.Id;
+import aar226_akc55_ayc62_ahl88.newast.stmt.Block;
 import java.util.ArrayList;
 
 public class Method extends Definition {
@@ -13,17 +15,17 @@ public class Method extends Definition {
 
     public Method(String s, ArrayList<Decl> d, ArrayList<Type> t, Block b, int l, int c){
         super (l,c);
-        for (Decl cur: d){
-            if (!cur.type.dimensions.allEmpty) {
-                throw new Error("array in param list has init value");
-            }
-        }
+//        for (Decl cur: d){
+//            if (!cur.type.dimensions.allEmpty) {
+//                throw new Error("array in param list has init value");
+//            }
+//        }
         for (Type cur: t){
             if (!cur.dimensions.allEmpty) {
                 throw new Error("array in type list has init value");
             }
         }
-        id = new Id(s);
+        id = new Id(s, l, c);
         decls = d;
         types = t;
         block = b;
