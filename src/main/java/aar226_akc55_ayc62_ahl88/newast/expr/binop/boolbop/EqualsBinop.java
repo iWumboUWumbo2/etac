8718cb79ddad266f77558f4a60dd8e8f2f825023
alanli2001 <alanli2001@users.noreply.github.com>
2023-02-21 16:53:02 -0500

    =,!= typecheck refactor

--- src/main/java/aar226_akc55_ayc62_ahl88/newast/expr/binop/boolbop/EqualsBinop.java
+++ src/main/java/aar226_akc55_ayc62_ahl88/newast/expr/binop/boolbop/EqualsBinop.java
@@ -19,42 +19,7 @@ public class EqualsBinop extends EquivalenceBinop {
         super(BinopEnum.EQUALS, in1, in2, l, c);
     }
 
-    @Override
     public Type typeCheck(SymbolTable s) throws Error {
-        Expr e1 = getLeftExpr();
-        Expr e2 = getRightExpr();
-        Type t1 = getLeftExpr().typeCheck(s);
-        Type t2 = getRightExpr().typeCheck(s);
-        String message;
-
-        if (t1.getType() == Type.TypeCheckingType.INT) {
-            if (t2.getType() != Type.TypeCheckingType.INT) {
-                message = Integer.toString(e2.getLine())
-                        + ":" + Integer.toString(e2.getColumn())
-                        + "  TypeError: plus e2 does not match e1 ";
-            } else {
-                return new Type(Type.TypeCheckingType.INT);
-            }
-        } else if (t1.getType() == Type.TypeCheckingType.INTARRAY) {
-            if (t2.getType() != Type.TypeCheckingType.INTARRAY) {
-                message = Integer.toString(e2.getLine())
-                        + ":" + Integer.toString(e2.getColumn())
-                        + "  TypeError: plus e2 does not match e1 ";
-            } else {
-                return new Type(Type.TypeCheckingType.INTARRAY);
-            }
-        } else if (t1.getType() == Type.TypeCheckingType.BOOLARRAY) {
-            if (t2.getType() != Type.TypeCheckingType.BOOLARRAY) {
-                message = Integer.toString(e2.getLine())
-                        + ":" + Integer.toString(e2.getColumn())
-                        + "  TypeError: plus e2 does not match e1 ";
-            } else {
-                return new Type(Type.TypeCheckingType.BOOLARRAY);
-            }
-        }
-        message = Integer.toString(e1.getLine())
-                + ":" + Integer.toString(e1.getColumn())
-                + "  TypeError: plus invalid e1 type ";
-        throw new Error(message);
+        return super.typeCheck(s);
     }
 }
