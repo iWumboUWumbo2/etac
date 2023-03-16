bf8eaf5e880062256d947e7f837d0a5cfef2d080
Arun Raman <aar226@cornell.edu>
2023-03-15 20:35:26 -0400

    fixed recursion1.eta and replaced xi alloc with eta alloc

--- src/main/java/aar226_akc55_ayc62_ahl88/visitors/IRVisitor.java
+++ src/main/java/aar226_akc55_ayc62_ahl88/visitors/IRVisitor.java
@@ -121,7 +121,7 @@ public class IRVisitor implements Visitor<IRNode>{
 
         String head_pointer = nxtTemp();
         // CALL(NAME(malloc), size)
-        IRCallStmt alloc_call = new IRCallStmt(new IRName("_xi_alloc"), 1L, malloc_size);
+        IRCallStmt alloc_call = new IRCallStmt(new IRName("_eta_alloc"), 1L, malloc_size);
 //        IRMove malloc_move = new IRMove(new IRTemp(head_pointer),alloc_call);
         IRSeq malloc_move = new IRSeq(alloc_call,new IRMove(new IRTemp(head_pointer), new IRTemp("_RV1")));
 
@@ -409,7 +409,7 @@ public class IRVisitor implements Visitor<IRNode>{
                 new IRConst(WORD_BYTES));
 
         // CALL(NAME(malloc), size)
-        IRCallStmt alloc_call = new IRCallStmt(new IRName("_xi_alloc"),1L, size);
+        IRCallStmt alloc_call = new IRCallStmt(new IRName("_eta_alloc"),1L, size);
 
         // reg[t] <- call malloc
         IRMove malloc_move = new IRMove(new IRTemp(t), new IRTemp("_RV1"));
@@ -838,7 +838,7 @@ public class IRVisitor implements Visitor<IRNode>{
                 new IRConst(WORD_BYTES));
 
         // call alloc and move RV1 into val
-        IRCallStmt alloc_call1 = new IRCallStmt(new IRName("_xi_alloc"),1L, size1);
+        IRCallStmt alloc_call1 = new IRCallStmt(new IRName("_eta_alloc"),1L, size1);
         IRSeq malloc_move1 = new IRSeq(alloc_call1,new IRMove(new IRTemp(tm), new IRTemp("_RV1")));
 
         // move len into -1
