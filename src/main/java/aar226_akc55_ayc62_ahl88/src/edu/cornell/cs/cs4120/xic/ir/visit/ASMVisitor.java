f5affc7649d07dae3c3dadb42c4a1a70f73bc308
Angela Cui <ayc62@cornell.edu>
2023-03-29 17:48:19 -0400

    notes

--- src/main/java/aar226_akc55_ayc62_ahl88/src/edu/cornell/cs/cs4120/xic/ir/visit/ASMVisitor.java
+++ src/main/java/aar226_akc55_ayc62_ahl88/src/edu/cornell/cs/cs4120/xic/ir/visit/ASMVisitor.java
@@ -1,4 +1,43 @@
 package aar226_akc55_ayc62_ahl88.src.edu.cornell.cs.cs4120.xic.ir.visit;
 
+/**
+ * NOTES:
+ * - branches: use cmp then corresponding jump
+ * - multireturn: store first argument in rax, push rest on stack
+ * - function args: rdi, rsi, rdx, rcx, r8, r9, push rest on stack
+ * - callee saved: rbx, rbp, and r12–r15
+ * - caller saved: everything else
+ *
+ * THINGS WE NEEEEEEEEEEEDDDDDDDDD
+ * - ir compunit ->
+ *      add glob mem locations
+ *      call visit to all functions
+ * - ir func decl ->
+ *      create new label at the top for function name
+ *      follow abi for args and register alloc
+ *      call visit on each statement
+ *      flatten arraylist we get back
+ *      follow abi for correct func exit
+ * - jump -> jr <label name>
+ * - cjump ->
+ *      two args: cmp <t1> <t2>; j<flag> <label name>
+ *      if instance of IRConst and is boolliteral: true -> jr / false -> nothing
+ * - move ->
+ *      mooooooooooooove
+ *      contains expr: used for dynamic tiling
+ *      tiling
+ * - mem
+ * - binop
+ * - call stmt ->
+ *      init function call follow abi for function call (store func args correctly)
+ *      jump to function
+ *      clean accroding to ABI spec (be careful about multireturns and stack dead space)
+ *      pop from function
+ * - IR Label -> ASM label
+ *
+ *
+ * Generic ASM Class will be called ASMNode
+ */
+
 public class ASMVisitor {
 }
