c30a047b7adc0a0540c1ce931645885a7d6cba4b
Andrew <akc55@cornell.edu>
2023-03-30 09:35:39 -0400

    added andy opcodes

--- src/main/java/aar226_akc55_ayc62_ahl88/asm/OpCodes.java
+++ src/main/java/aar226_akc55_ayc62_ahl88/asm/OpCodes.java
@@ -2,9 +2,21 @@ package aar226_akc55_ayc62_ahl88.asm;
 
 public class OpCodes {
     public enum BinOp {
-        ADD,
-        SUB,
-        IMUL
+        // 0 arg
+
+        // 1 arg
+        JMP, // jump always
+        JE, // jump if equal a == b
+        JNE, // jump if not equal a != b
+        JL, // jump if less than a < b
+        JLE, // jump if less than equal a <= b
+        JG, //jump if greater a > b
+        JGE, // jump if greater equal a >= b
+        CALL, // call it when you call a function call stmt
+        RET, // when you finish executing function do ret.
+
+        // 2 arg
+
     }
 
     public enum Jump {
