3359dc15fc9a471ffa3558cedef979d6033123b9
alanli2001 <ahl88@cornell.edu>
2023-03-31 01:37:37 -0400

    added asmarg2

---
+++ src/main/java/aar226_akc55_ayc62_ahl88/asm/ASMArg2.java
@@ -0,0 +1,12 @@
+package aar226_akc55_ayc62_ahl88.asm;
+
+public class ASMArg2 extends ASMInstruction{
+
+    ASMExpr left;
+    ASMExpr right;
+    public ASMArg2(ASMOpCodes op, ASMExpr arg1, ASMExpr arg2){
+        super(op);
+        left = arg1;
+        right = arg2;
+    }
+}
