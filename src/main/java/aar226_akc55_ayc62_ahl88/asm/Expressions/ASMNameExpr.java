a3d010b4959b394063b21f71ea8aed197ff613cc
alanli2001 <alanli2001@users.noreply.github.com>
2023-03-31 18:11:56 -0400

    add asmnameexpr

--- src/main/java/aar226_akc55_ayc62_ahl88/asm/Expressions/ASMNameExpr.java
+++ src/main/java/aar226_akc55_ayc62_ahl88/asm/Expressions/ASMNameExpr.java
@@ -1,6 +1,8 @@
 package aar226_akc55_ayc62_ahl88.asm.Expressions;
 
 public class ASMNameExpr extends ASMExpr {
+    private String name;
     public ASMNameExpr(String name) {
+        this.name = name;
     }
 }
