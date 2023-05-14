19bb15609e3acf737c21ee17c0696d024f361730
alanli2001 <alanli2001@users.noreply.github.com>
2023-05-14 15:59:19 -0400

    more cleanup

--- src/main/java/aar226_akc55_ayc62_ahl88/newast/AstNode.java
+++ src/main/java/aar226_akc55_ayc62_ahl88/newast/AstNode.java
@@ -1,10 +1,6 @@
 package aar226_akc55_ayc62_ahl88.newast;
 
-//import aar226_akc55_ayc62_ahl88.SymbolTable.SymbolTable;
-import aar226_akc55_ayc62_ahl88.SymbolTable.SymbolTable;
 import aar226_akc55_ayc62_ahl88.src.edu.cornell.cs.cs4120.util.CodeWriterSExpPrinter;
-import aar226_akc55_ayc62_ahl88.src.edu.cornell.cs.cs4120.xic.ir.IRNode;
-import aar226_akc55_ayc62_ahl88.visitors.IRVisitor;
 
 /**
  * Abstract Class for AST nodes
@@ -13,6 +9,8 @@ public abstract class AstNode implements Printer {
     int line;
     int col;
     protected Type nodeType;
+    public int getLine(){return line;}
+    public int getColumn(){return col;}
 
     /**
      * @param l line number
@@ -22,18 +20,7 @@ public abstract class AstNode implements Printer {
         line = l;
         col = c;
     }
-
-    /**
-     * @return Return line number for element
-     */
-    public int getLine(){return line;}
-
-    /**
-     * @return Return starting column number for element
-     */
-    public int getColumn(){return col;}
     public abstract void prettyPrint(CodeWriterSExpPrinter p);
-
     public Type getNodeType(){
         return nodeType;
     }
