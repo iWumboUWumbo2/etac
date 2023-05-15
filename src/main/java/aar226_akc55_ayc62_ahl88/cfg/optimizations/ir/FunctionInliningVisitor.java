5eea8ca4118b460cd7ee3e95a10329a086c34215
Andrew <akc55@cornell.edu>
2023-05-15 19:50:29 -0400

    removed inline removal

--- src/main/java/aar226_akc55_ayc62_ahl88/cfg/optimizations/ir/FunctionInliningVisitor.java
+++ src/main/java/aar226_akc55_ayc62_ahl88/cfg/optimizations/ir/FunctionInliningVisitor.java
@@ -127,11 +127,6 @@ public class FunctionInliningVisitor implements IROPTVisitor<IRNode> {
         }
 
         HashMap<String,IRFuncDecl> reorderedBlocks = reorderFunctions(newIrFunc);
-        for (String inline: allowedInline){
-            if (!inline.equals("_Imain_paai")) {
-                reorderedBlocks.remove(inline);
-            }
-        }
         return new IRCompUnit(node.name(), reorderedBlocks,new ArrayList<>(),node.dataMap());
     }
 
